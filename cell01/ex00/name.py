fc403c8180c4657ab0511d814fe9cd080b769759
SawZiDunn <szidunn199@gmail.com>
2025-06-23 13:31:10 +0700

    add cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Saw"
+last_name = "Zi Dunn"
+
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Saw"
+last_name = "Zi Dunn"
+whole_name = first_name + " " + last_name
+
+print(whole_name)
